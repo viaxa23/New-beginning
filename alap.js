1a8b9df0029aae52dd8824f210d9a97470d5232e
Visnyei Ádám <visnyei.adam@sob.lidice.local>
2020-09-10 09:26:57 +0200

    harmadik kommit

---
+++ alap.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let nev = 'Visnyei Ádám';
+
+/* Moduláris ablak függvény */
+// window.alert(nev);
+// alert(nev);
+
+//let tanuloSzam = window.prompt('Hány tanuló van jelen:', 12 );
+//let szoveg = `A tanulók száma: ${tanuloSzam}`;
+
+let szoveg = 'Hello World';
+//szoveg = null;
+//window.alert(szoveg);
+
+document.getElementById('kiir').innerText = szoveg;
